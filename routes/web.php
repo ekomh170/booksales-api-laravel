721be6db326bfe02ab21f19ddb6115ae66b468b2
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2025-10-16 10:17:06 +0700

    feat: Update welcome endpoint dengan daftar endpoint Pertemuan 4

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Route;
  * Web Routes
  *
  * Route untuk web sudah dipindahkan ke routes/api.php
- * Aplikasi sekarang fokus ke REST API (Tugas Pertemuan 3)
+ * Aplikasi sekarang fokus ke REST API
+ * - Pertemuan 3: REST API Read Operations
+ * - Pertemuan 4: CRUD Create & Read untuk Genre dan Author
  */
 
 Route::get('/', function () {
@@ -16,11 +18,17 @@ Route::get('/', function () {
         'info' => [
             'nama' => 'Eko Muchamad Haryono',
             'nim' => '0110223079',
-            'tugas' => 'Pertemuan 3 - REST API',
+            'pertemuan' => [
+                'Pertemuan 3' => 'REST API - Read Operations',
+                'Pertemuan 4' => 'CRUD API - Create & Read untuk Genre dan Author'
+            ],
             'endpoints' => [
+                'GET /api/genres' => 'Mendapatkan semua genres',
+                'POST /api/genres' => 'Membuat genre baru',
                 'GET /api/authors' => 'Mendapatkan semua authors',
-                'GET /api/books' => 'Mendapatkan semua books',
                 'GET /api/authors/{id}' => 'Mendapatkan detail author',
+                'POST /api/authors' => 'Membuat author baru',
+                'GET /api/books' => 'Mendapatkan semua books',
                 'GET /api/books/{id}' => 'Mendapatkan detail book',
             ]
         ]
